69faacbacb257182be2ff332d588c0845743cf82
Julia Szymczak <ju.szymczak@gmail.com>
2020-11-16 12:46:32 +0100

    final all problems and completed project

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 import static readability.lettersHandler.isVowel;
